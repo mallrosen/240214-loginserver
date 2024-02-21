7d409ca84c0bdf4c8d4905a2052aadde9307eded
malin <malin.rosen@medieinstitutet.se>
2024-02-21 09:48:00 +0100

    dotenv

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,36 @@
 const { UserAccount } = require("../models");
 const bcrypt = require('bcrypt')
 
-function onHej(req, res){
+async function onHej(req, res){
+    const id = req.session.userId
+    const user = await UserAccount.findOne({
+        where: {id}
+    })
+
+    res.json(user)
     res.send('Hejsan')
 }
 
-function onLogin(req, res){
-    res.send('Hejsan')
+async function onLogin(req, res){
+    //1. Ta lösenordet och email från req.body
+    //2. lösenordet bcryptas och jämförs med det i databasen
+    //3. Dks koppling i session storage
+    // mappa cookie -> useraccount.id
+    const {email,password} = req.body
+
+    const user = await UserAccount.findOne({
+        where: {email}
+    });
+    if (!user) {
+        return res.status(401).json('Could not login');
+    }
+    const passwordValid = await bcrypt.compare(password, user.password);
+    if (!passwordValid) {
+        return res.status(401).json('Could not login');
+        //HA SAMMA FELMEDDELANDE FÖR ATT GÖRA DET SVÅRARE FÖR EN HACKER. SÄKERHET
+    }
+    req.session.userId = user.id
+     res.json({status:"Yepp"})
 }
 
 async function onCreateUser(req, res){
@@ -23,6 +47,8 @@ async function onCreateUser(req, res){
     //Vem är inloggad?? 
     res.status(204).json({email})
 }
+
+
 
 module.exports = {
     onHej,
--- main.js
+++ main.js
@@ -12,27 +12,43 @@ const userController = require("./controllers/userController");
 const migrationhelper = require("./migrationhelper");
 // const { onCreateUser } = require('./config/controllers/userController');
 
+
+//Det under är vem som får "prata" med oss. 
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5500",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
 
+app.use(
+    session({
+      secret: "my-secret-key",
+      resave: false,
+      saveUninitialized: true,
+      // cookie: { secure: true }
+    })
+  );
+  
+
 app.get('/hej', userController.onHej)
 app.post('/api/useraccount', userController.onCreateUser);
 
+
+app.post('/api/signin', userController.onLogin)
+
+
+//GET http://localhost:3000/test?name=Malin
+//som ligger i calls.rest är kopplat till den under
+
+app.get('/test', (req, res) => {
+  const {name} = req.query;
+  return res.send(`<html><body>Hello, ${name}!</body></html>`)
+})
+
 //session är en funktion () om man ser cookie längst ner är det ett objekt
 
-app.use(
-  session({
-    secret: "my-secret-key",
-    resave: false,
-    saveUninitialized: true,
-    // cookie: { secure: true }
-  })
-);
 
 app.get("/hej", (req, res) => {
   //Cookie:n och vem är inloggad? Det är det som är req!
